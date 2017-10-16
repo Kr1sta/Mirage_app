604de0dd7ecefca7825594f003fd71876ee0e984
Krista Laffrenier <kristalaffrenier@gmail.com>
2017-10-16 17:47:42 -0400

    complete design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to root_path
+      flash[:success] = "Thank you for registering with Mirage! We will get back to you as soon as possible!"
     else
       render 'new'
+      flash[:error] = "It looks like something went wrong with your registration. Please try again"
     end
   end
 
